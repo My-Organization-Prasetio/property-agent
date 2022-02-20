293c058f61bb8bd5b38738b7286e3598b43aa2c6
Dedi Prasetio <dediprasetio03@gmail.com>
2022-02-20 15:20:12 +0700

    menu master to group tambahan

--- application/views/admin/template/sidebar_v.php
+++ application/views/admin/template/sidebar_v.php
@@ -12,7 +12,13 @@
         </a>
     </li>
     <li class="nav-item"><a href="<?= base_url() ?>service" class="nav-link disabled"><i class="icon-wall icon"></i>Transaksi & Layanan <span class="badge bg-transparent align-self-center ml-auto">Segera Hadir</span></a></li> 
-    <li class="nav-item"><a href="<?= base_url() ?>asset-management" class="nav-link disabled"><i class="icon-office"></i>Manajemen Aset <span class="badge bg-transparent align-self-center ml-auto">Segera Hadir</span></a></li>  
+    <li class="nav-item"><a href="<?= base_url() ?>asset-management" class="nav-link"><i class="icon-office"></i>Manajemen Aset</a></li>  
+    <li class="nav-item"><a href="<?= base_url() ?>admin/report" class="nav-link disabled"><i class="icon-file-presentation2 icon"></i> Laporan <span class="badge bg-transparent align-self-center ml-auto">Segera Hadir</span></a></li>  
+
+    <!-- Tambahan -->
+    <li class="nav-item-header mt-0">
+        <div class="text-uppercase font-size-xs line-height-xs">Tambahan</div> <i class="icon-menu" title="Main"></i>
+    </li>
     <li class="nav-item nav-item-submenu">
         <a href="#" class="nav-link"><i class="icon-grid icon"></i> <span>Master</span></a>
         <ul class="nav nav-group-sub" data-submenu-title="Themes">
@@ -23,12 +29,6 @@
             <!-- <li class="nav-item"><a href="<?= base_url() ?>admin/employee" class="nav-link">Karyawan</a></li> -->
             <li class="nav-item"><a href="<?= base_url() ?>master/material-category" class="nav-link disabled">Segera Hadir<span class="badge bg-transparent align-self-center ml-auto">Coming soon</span></a></li>
         </ul>
-    </li>
-    <li class="nav-item"><a href="<?= base_url() ?>admin/report" class="nav-link disabled"><i class="icon-file-presentation2 icon"></i> Laporan <span class="badge bg-transparent align-self-center ml-auto">Segera Hadir</span></a></li>  
-
-    <!-- Tambahan -->
-    <li class="nav-item-header mt-0">
-        <div class="text-uppercase font-size-xs line-height-xs">Tambahan</div> <i class="icon-menu" title="Main"></i>
     </li>
     <li class="nav-item"><a href="<?= base_url() ?>admin/company" class="nav-link"><i class="icon-office"></i>Tentang Perusahaan</a></li>
     <li class="nav-item"><a href="<?= base_url() ?>admin/user" class="nav-link"><i class="icon-people"></i>Pengguna</a></li>
